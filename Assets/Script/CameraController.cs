5da50b52e9b6d2f58b86ffe99c934e5779c3091b
Vinicius <vinicius.matos@estudante.ufjf.br>
2025-03-17 15:06:40 -0300

    Adiciona terreno básico

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -14,8 +14,8 @@ public class CameraController : MonoBehaviour
     public void Start()
     {
         camera = GetComponentInChildren<Camera>();
-         Cursor.lockState = CursorLockMode.Locked;
-         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void RotateCamera(Vector2 input)
@@ -27,7 +27,6 @@ public class CameraController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSense);
-
 
     }
 
